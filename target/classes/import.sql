a18eafb0a1436478a504c519fa5db8ab1ece27f8
alkfejl <balazs.arvay@gmail.com>
2017-10-29 11:40:58 +0100

    Kezdes

---
+++ target/classes/import.sql
@@ -0,0 +1,11 @@
+insert into student (first_name, last_name, neptun) values ('John', 'Wick', 'abcdef1');
+insert into student (first_name, last_name, neptun) values ('John', 'Doe', 'abcdef2');
+insert into student (first_name, last_name, neptun) values ('Johnathan', 'Apple', 'abcdef3');
+insert into student (first_name, last_name, neptun) values ('Bob', null, 'abcdef4');
+insert into student (first_name, last_name, neptun) values ('Ian', 'Thorne', 'abcdef5');
+
+insert into user (email, password, role) values ('admin@email.hu', 'admin', 'admin');
+insert into user (email, password, role) values ('club1@email.hu', 'club1', 'club_master');
+insert into user (email, password, role) values ('club2@email.hu', 'club1', 'club_master');
+insert into user (email, password, role) values ('band1@email.hu', 'band1', 'band_master');
+insert into user (email, password, role) values ('band2@email.hu', 'band2', 'band_master');
